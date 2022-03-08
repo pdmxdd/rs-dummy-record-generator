183d7348bda545533b800fd9b1949c9f346a46d7
julia <pdmxdd@gmail.com>
2022-03-07 23:04:28 -0600

    to struct!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use fake::locales::EN;
 use fake::Fake;
+use fake::faker::name::raw::*;
+use fake::faker::internet::raw::*;
+
+pub struct User {
+    first_name: String,
+    last_name: String,
+    username: String,
+}
+
+impl User {
+    fn new() -> Self {
+        User {
+            first_name: FirstName(EN).fake(),
+            last_name: LastName(EN).fake(),
+            username: Username(EN).fake(),
+        }
+    }
+}
+
 
 fn main() {
-    use fake::faker::name::raw::*;
-    let first_name: String = FirstName(EN).fake();
-    let last_name: String = LastName(EN).fake();
-    println!("first: {}\nlast: {}", first_name, last_name);
+    let user: User = User::new();
+    println!("first_name: {}\nlast_name: {}\nusername: {}", user.first_name, user.last_name, user.username);
 }
