0dbc342ae268b1317f80df16d986bd1c04e729a4
julia <pdmxdd@gmail.com>
2022-03-07 23:35:38 -0600

    random number for user.balance

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,37 @@ use fake::locales::EN;
 use fake::Fake;
 use fake::faker::name::raw::*;
 use fake::faker::internet::raw::*;
+use rand::Rng;
 
 pub struct User {
     first_name: String,
     last_name: String,
     username: String,
+    balance: f64,
 }
 
 impl User {
-    fn new() -> Self {
-        User {
+    pub fn new() -> Self {
+        Self {
             first_name: FirstName(EN).fake(),
             last_name: LastName(EN).fake(),
             username: Username(EN).fake(),
+            balance: rand::thread_rng().gen_range(1.00..25000.00),
         }
     }
 }
 
+/*
+impl std::fmt::Display for User {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "first_name: {}\nlast_name: {}\nusername: {}\nbalance: {}", self.first_name, self.last_name, self.username, self.balance);
+    }
+}
+*/
+
 
 fn main() {
     let user: User = User::new();
-    println!("first_name: {}\nlast_name: {}\nusername: {}", user.first_name, user.last_name, user.username);
+    // println!(user);
+    println!("first_name: {}\nlast_name: {}\nusername: {}\nbalance: {}", user.first_name, user.last_name, user.username, user.balance);
 }
