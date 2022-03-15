233ccd76ded39c766522e5ea3d0d5db968b36a2e
julia <pdmxdd@gmail.com>
2022-03-14 21:53:09 -0500

    migrated random_password to be used by User::new()

--- src/generate.rs
+++ src/generate.rs
@@ -3,12 +3,34 @@ use fake::Fake;
 use fake::faker::name::raw::{FirstName, LastName};
 use fake::faker::internet::raw::{Username};
 use rand::Rng;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn random_password() -> String {
+    if let Ok(mut lines) = read_lines("10-million-password-list-top-1000000.txt") {
+        let password = lines.nth(rand::thread_rng().gen_range(0..999997))
+            .expect("File does not exist")
+            .expect("File empty")
+            .to_string();
+        let password = &password;
+        return password.to_string();
+        // return lines.nth(rand::thread_rng().gen_range(0..999997)).unwrap().unwrap().to_string();
+    }
+    return "".to_string();
+}
 
 pub struct User {
     pub first_name: String,
     pub last_name: String,
     pub username: String,
     pub balance: f64,
+    pub password: String,
 }
 
 impl User {
@@ -18,6 +40,7 @@ impl User {
             last_name: LastName(EN).fake(),
             username: Username(EN).fake(),
             balance: rand::thread_rng().gen_range(1.00..7500.00),
+            password: random_password(),
         }
     }
 }
