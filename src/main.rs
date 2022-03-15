b8b240f5ea80c42a9c12894efc38cb607e1a8139
julia <pdmxdd@gmail.com>
2022-03-14 21:36:01 -0500

    random password

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,36 @@ mod write_users;
 
 use write_users::write_users_to_csv;
 
-fn main() {    
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+use rand::Rng;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn random_password() -> String {
+    if let Ok(mut lines) = read_lines("10-million-password-list-top-1000000.txt") {
+        let password = lines.nth(rand::thread_rng().gen_range(0..999997)).unwrap().unwrap().to_string();
+        let password = &password[0..password.len()];
+        return password.to_string();
+        // return lines.nth(rand::thread_rng().gen_range(0..999997)).unwrap().unwrap().to_string();
+    }
+    return "".to_string();
+}
+
+fn main() { 
+    
+    println!("{}", random_password());
+
+    // if let Ok(mut lines) = read_lines("10-million-password-list-top-1000000.txt") {
+    //     // number of records 999997
+    //     println!("{:?}", &lines.nth(rand::thread_rng().gen_range(0..999997)).unwrap().unwrap().to_string());
+    // }
+
     // let user: User = User::new();
     // println!("first_name: {}\nlast_name: {}\nusername: {}\nbalance: {}", user.first_name, user.last_name, user.username, user.balance);
     
