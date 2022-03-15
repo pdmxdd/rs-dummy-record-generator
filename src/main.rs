06fb3f97c7263ea8bc87d2af293137575f22b37e
julia <pdmxdd@gmail.com>
2022-03-14 20:35:00 -0500

    commented out code in main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::process;
+use std::process::Command;
 
 mod generate;
 use generate::User;
@@ -12,22 +13,25 @@ mod write_users;
 use write_users::write_users_to_csv;
 
 fn main() {
-    let user: User = User::new();
-    println!("first_name: {}\nlast_name: {}\nusername: {}\nbalance: {}", user.first_name, user.last_name, user.username, user.balance);
-    /*
-    if let Err(err) = example() {
-        println!("error running example: {}", err);
-        process::exit(1);
-    }
-    */
-    
-    if let Err(err) = read_users_from_csv() { 
-        println!("error running example: {}", err);
-        process::exit(1);
-    }
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("echo hello")
+        .output()
+        .expect("failed to execute process");
 
-    if let Err(err) = write_users_to_csv() {
-        println!("error running example: {}", err);
-        process::exit(1);
-    }
+    let hello = output.stdout;
+    println!("{:?}", hello);
+    
+    // let user: User = User::new();
+    // println!("first_name: {}\nlast_name: {}\nusername: {}\nbalance: {}", user.first_name, user.last_name, user.username, user.balance);
+    
+    // if let Err(err) = read_users_from_csv() { 
+    //     println!("error running example: {}", err);
+    //     process::exit(1);
+    // }
+
+    // if let Err(err) = write_users_to_csv() {
+    //     println!("error running example: {}", err);
+    //     process::exit(1);
+    // }
 }
